bb0d2e09c6ea2bdfe5adc8f0bec9fe0f390b87d0
Luigiket <luigiket@student.21-school.ru>
2023-06-20 18:06:11 +0300

    need to fix parser for some pages

--- get_html.py
+++ get_html.py
@@ -1,46 +1,113 @@
+import aiohttp
+import aiofile
+import asynch
+import asyncio
+import lxml
+from fake_useragent import UserAgent
+from bs4 import BeautifulSoup
 import requests
+
+ua = UserAgent()
 
 cookies = {
     'lang': 'ru',
-    'PHPSESSID': '412740a6ca76ba3d8ff309a81b4e0b26',
-    'cartUserCookieIdent_v3': 'b611419eff1d5d4acb2ba01148e2be4a1311832507f17fcc7b309c4b8a781638a%3A2%3A%7Bi%3A0%3Bs%3A22%3A%22cartUserCookieIdent_v3%22%3Bi%3A1%3Bs%3A36%3A%2242a1b947-2e5c-33ba-ac36-9e2e47956d22%22%3B%7D',
-    'phonesIdent': 'd24ba292f28de045ad955d5e6a4430377b095f41b19b8e6320a56fc8e685a3b2a%3A2%3A%7Bi%3A0%3Bs%3A11%3A%22phonesIdent%22%3Bi%3A1%3Bs%3A36%3A%22e4c18612-e9cc-40cc-b1b8-3b2256bc08c0%22%3B%7D',
-    '_gid': 'GA1.2.1433954782.1687110980',
-    '_gcl_au': '1.1.1776523216.1687110981',
-    '_ym_uid': '1687110982982147384',
-    '_ym_d': '1687110982',
-    'tmr_lvid': '4496557623f7a3a2aaac969e5f41d137',
-    'tmr_lvidTS': '1687110982546',
+    'city_path': 'moscow',
+    'phonesIdent': '4e957da8c854eddd1206b107327b2639a48bda9c0376c16fa93677d277038e54a%3A2%3A%7Bi%3A0%3Bs%3A11%3A%22phonesIdent%22%3Bi%3A1%3Bs%3A36%3A%22ebc14c93-0de7-4b96-81db-cca4277fefa1%22%3B%7D',
+    'cartUserCookieIdent_v3': '066ea3df7194c39af1c33108f73e99305445267de595e9d936bf1da6d0695758a%3A2%3A%7Bi%3A0%3Bs%3A22%3A%22cartUserCookieIdent_v3%22%3Bi%3A1%3Bs%3A36%3A%22e1838e05-bce1-3d5d-9639-1f76a4c394e5%22%3B%7D',
+    '_gcl_au': '1.1.1769402233.1687152784',
+    'rrpvid': '514393665157779',
+    'PHPSESSID': '77a3d6063def3e5ebd1ae05913401664',
+    'tmr_lvid': 'd31b8ac6291611090dbad5635c274b09',
+    'tmr_lvidTS': '1687152784780',
+    '_ym_uid': '1687152785781607891',
+    '_ym_d': '1687152785',
+    'rcuid': '648d7872ef0b6f85d3a3e9ef',
+    'current_path': '605bfdc517d7e9e23947448a9bf1ce16ac36b884434a3fdb10db053793c50392a%3A2%3A%7Bi%3A0%3Bs%3A12%3A%22current_path%22%3Bi%3A1%3Bs%3A115%3A%22%7B%22city%22%3A%2230b7c1f3-03fb-11dc-95ee-00151716f9f5%22%2C%22cityName%22%3A%22%5Cu041c%5Cu043e%5Cu0441%5Cu043a%5Cu0432%5Cu0430%22%2C%22method%22%3A%22manual%22%7D%22%3B%7D',
+    '_csrf': '2215566cad29db7d4b82817167355e5a41de8f04d7183110890bbb19874b7586a%3A2%3A%7Bi%3A0%3Bs%3A5%3A%22_csrf%22%3Bi%3A1%3Bs%3A32%3A%22EVOgtvpxzikgAfqy5rdvNj5er36VAV4v%22%3B%7D',
+    '_gid': 'GA1.2.1759671824.1687265202',
     '_ym_isad': '2',
-    'city_path': 'kazan',
-    'current_path': '0e2725c9274cf653d41cb76144c4d7fafe50cd9cbb28f12b7003ce57fae1839ca%3A2%3A%7Bi%3A0%3Bs%3A12%3A%22current_path%22%3Bi%3A1%3Bs%3A115%3A%22%7B%22city%22%3A%2255506b55-0565-11df-9cf0-00151716f9f5%22%2C%22cityName%22%3A%22%5Cu041a%5Cu0430%5Cu0437%5Cu0430%5Cu043d%5Cu044c%22%2C%22method%22%3A%22manual%22%7D%22%3B%7D',
-    '_csrf': '78f159e2d690d0bd00f696141570356f692dea0ae5aea87841663929f145e211a%3A2%3A%7Bi%3A0%3Bs%3A5%3A%22_csrf%22%3Bi%3A1%3Bs%3A32%3A%222ezIg_M7byXpSJv5YO2-WsWp64tDwnL2%22%3B%7D',
-    'rrpvid': '153457539511767',
-    'rcuid': '648f45779144efaeaf29dda6',
-    'qrator_jsid': '1687110960.800.vlwEeE8id7ZugpIK-v4qcsee877jg79g95ce3ss1mbg75abvp',
-    '_ga_FLS4JETDHW': 'GS1.1.1687113503.2.0.1687113503.60.0.0',
-    '_ga': 'GA1.2.915581245.1687110980',
     '_ym_visorc': 'b',
-    'tmr_detect': '0%7C1687113507269',
+    'qrator_jsid': '1687271154.470.EW5vphBa1eEDM4Vc-ldassv1j90b9gf65te3r7jjivjef6dag',
+    'rr-testCookie': 'testvalue',
+    '_gat': '1',
+    '_gat_%5Bobject%20Object%5D': '1',
+    '_ga_FLS4JETDHW': 'GS1.1.1687271156.5.1.1687273077.59.0.0',
+    '_ga': 'GA1.1.487180927.1687152784',
+    'tmr_detect': '0%7C1687273079597',
 }
 
 headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Accept-Language': 'en-US,en;q=0.9,ru;q=0.8',
     'Cache-Control': 'max-age=0',
     'Connection': 'keep-alive',
-    # 'Cookie': 'lang=ru; PHPSESSID=412740a6ca76ba3d8ff309a81b4e0b26; cartUserCookieIdent_v3=b611419eff1d5d4acb2ba01148e2be4a1311832507f17fcc7b309c4b8a781638a%3A2%3A%7Bi%3A0%3Bs%3A22%3A%22cartUserCookieIdent_v3%22%3Bi%3A1%3Bs%3A36%3A%2242a1b947-2e5c-33ba-ac36-9e2e47956d22%22%3B%7D; phonesIdent=d24ba292f28de045ad955d5e6a4430377b095f41b19b8e6320a56fc8e685a3b2a%3A2%3A%7Bi%3A0%3Bs%3A11%3A%22phonesIdent%22%3Bi%3A1%3Bs%3A36%3A%22e4c18612-e9cc-40cc-b1b8-3b2256bc08c0%22%3B%7D; _gid=GA1.2.1433954782.1687110980; _gcl_au=1.1.1776523216.1687110981; _ym_uid=1687110982982147384; _ym_d=1687110982; tmr_lvid=4496557623f7a3a2aaac969e5f41d137; tmr_lvidTS=1687110982546; _ym_isad=2; city_path=kazan; current_path=0e2725c9274cf653d41cb76144c4d7fafe50cd9cbb28f12b7003ce57fae1839ca%3A2%3A%7Bi%3A0%3Bs%3A12%3A%22current_path%22%3Bi%3A1%3Bs%3A115%3A%22%7B%22city%22%3A%2255506b55-0565-11df-9cf0-00151716f9f5%22%2C%22cityName%22%3A%22%5Cu041a%5Cu0430%5Cu0437%5Cu0430%5Cu043d%5Cu044c%22%2C%22method%22%3A%22manual%22%7D%22%3B%7D; _csrf=78f159e2d690d0bd00f696141570356f692dea0ae5aea87841663929f145e211a%3A2%3A%7Bi%3A0%3Bs%3A5%3A%22_csrf%22%3Bi%3A1%3Bs%3A32%3A%222ezIg_M7byXpSJv5YO2-WsWp64tDwnL2%22%3B%7D; rrpvid=153457539511767; rcuid=648f45779144efaeaf29dda6; qrator_jsid=1687110960.800.vlwEeE8id7ZugpIK-v4qcsee877jg79g95ce3ss1mbg75abvp; _ga_FLS4JETDHW=GS1.1.1687113503.2.0.1687113503.60.0.0; _ga=GA1.2.915581245.1687110980; _ym_visorc=b; tmr_detect=0%7C1687113507269',
-    'Sec-Fetch-Dest': 'empty',
+    # 'Cookie': 'lang=ru; city_path=moscow; phonesIdent=4e957da8c854eddd1206b107327b2639a48bda9c0376c16fa93677d277038e54a%3A2%3A%7Bi%3A0%3Bs%3A11%3A%22phonesIdent%22%3Bi%3A1%3Bs%3A36%3A%22ebc14c93-0de7-4b96-81db-cca4277fefa1%22%3B%7D; cartUserCookieIdent_v3=066ea3df7194c39af1c33108f73e99305445267de595e9d936bf1da6d0695758a%3A2%3A%7Bi%3A0%3Bs%3A22%3A%22cartUserCookieIdent_v3%22%3Bi%3A1%3Bs%3A36%3A%22e1838e05-bce1-3d5d-9639-1f76a4c394e5%22%3B%7D; _gcl_au=1.1.1769402233.1687152784; rrpvid=514393665157779; PHPSESSID=77a3d6063def3e5ebd1ae05913401664; tmr_lvid=d31b8ac6291611090dbad5635c274b09; tmr_lvidTS=1687152784780; _ym_uid=1687152785781607891; _ym_d=1687152785; rcuid=648d7872ef0b6f85d3a3e9ef; current_path=605bfdc517d7e9e23947448a9bf1ce16ac36b884434a3fdb10db053793c50392a%3A2%3A%7Bi%3A0%3Bs%3A12%3A%22current_path%22%3Bi%3A1%3Bs%3A115%3A%22%7B%22city%22%3A%2230b7c1f3-03fb-11dc-95ee-00151716f9f5%22%2C%22cityName%22%3A%22%5Cu041c%5Cu043e%5Cu0441%5Cu043a%5Cu0432%5Cu0430%22%2C%22method%22%3A%22manual%22%7D%22%3B%7D; _csrf=2215566cad29db7d4b82817167355e5a41de8f04d7183110890bbb19874b7586a%3A2%3A%7Bi%3A0%3Bs%3A5%3A%22_csrf%22%3Bi%3A1%3Bs%3A32%3A%22EVOgtvpxzikgAfqy5rdvNj5er36VAV4v%22%3B%7D; _gid=GA1.2.1759671824.1687265202; _ym_isad=2; _ym_visorc=b; qrator_jsid=1687271154.470.EW5vphBa1eEDM4Vc-ldassv1j90b9gf65te3r7jjivjef6dag; rr-testCookie=testvalue; _gat=1; _gat_%5Bobject%20Object%5D=1; _ga_FLS4JETDHW=GS1.1.1687271156.5.1.1687273077.59.0.0; _ga=GA1.1.487180927.1687152784; tmr_detect=0%7C1687273079597',
+    'Referer': 'https://www.dns-shop.ru/catalog/eb53394d63f878ee/noutbuki-i-aksessuary/',
+    'Sec-Fetch-Dest': 'document',
     'Sec-Fetch-Mode': 'navigate',
     'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-User': '?1',
     'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-    'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
+    'sec-ch-ua': '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"macOS"',
 }
 
-response = requests.get('https://www.dns-shop.ru/catalog/17a892f816404e77/noutbuki/', cookies=cookies, headers=headers)
+params = {
+    'p': '4',
+}
 
-with open('dns.html', 'w') as resp:
-    resp.write(response.text)
+
+temp = []
+
+async def get_page_data(session, page):
+    url = f"https://www.dns-shop.ru/catalog/17a892f816404e77/noutbuki/?p={page}"
+    async with session.get(url=url, headers=headers, cookies=cookies,) as response:
+        response_text = await response.text()
+        soup = BeautifulSoup(response_text, 'lxml')
+        
+        laptops = soup.find_all("div", class_="catalog-product ui-button-widget")
+        for _ in range(0, len(laptops)):
+            diagonal = laptops[_].find_all("span")[1].text.split()[0]
+            name = (laptops[_].find_all("span")[1].text.split()[2],laptops[_].find_all("span")[1].text.split()[3])
+            color = laptops[_].find_all("span")[1].text.split()[4]
+            matrix = (laptops[_].find_all("span")[1].text.split()[5],laptops[_].find_all("span")[1].text.split()[6])
+        
+            temp.append(
+                {
+                    "diagonal": diagonal,
+                    "name": name,
+                    "color": color,
+                    "matrix": matrix,
+                    "page": page
+                }
+            )
+            # print(diagonal)
+        # with open('dns.html', 'w') as resp:
+        #   resp.write(response_text)
+
+
+async def gather_data():
+    async with aiohttp.ClientSession() as session:
+        response = await session.get('https://www.dns-shop.ru/catalog/17a892f816404e77/noutbuki/', cookies=cookies, headers=headers)
+        soup = BeautifulSoup(await response.text(), 'lxml')
+        # pages_count = int(soup.find("div", class_="pagination-container").find_all("a")[-1].text)
+        tasks = []
+
+        for page in range(1, 4):
+            task = asyncio.create_task(get_page_data(session, page))
+            tasks.append(task)
+        
+        await asyncio.gather(*tasks)
+        # print(soup.text)
+
+
+def main():
+    asyncio.run(gather_data())
+    print(temp)
+
+
+if __name__ == '__main__':
+    main()
+
--- parsing.py
+++ parsing.py
@@ -1,25 +1,25 @@
-from bs4 import BeautifulSoup
-import csv
+# from bs4 import BeautifulSoup
+# import csv
 
-with open("dns.html") as file:
-    src = file.read()
+# with open("dns.html") as file:
+#     src = file.read()
     
-soup = BeautifulSoup(src, 'lxml')
+# soup = BeautifulSoup(src, 'lxml')
 
-laptops = soup.find_all("div", class_="catalog-product ui-button-widget")
+# laptops = soup.find_all("div", class_="catalog-product ui-button-widget")
 
-fields = ['diagonal', 'name', 'color']
-temp = []
+# fields = ['diagonal', 'name', 'color']
+# temp = []
 
-for _ in range(0, len(laptops)):
-    diagonal = laptops[_].find_all("span")[1].text.split()[0]
-    name = (laptops[_].find_all("span")[1].text.split()[2],laptops[_].find_all("span")[1].text.split()[3])
-    color = laptops[_].find_all("span")[1].text.split()[4]
-    temp.append(diagonal)
-    temp.append(name) 
-    temp.append(color)
+# for _ in range(0, len(laptops)):
+#     diagonal = laptops[_].find_all("span")[1].text.split()[0]
+#     name = (laptops[_].find_all("span")[1].text.split()[2],laptops[_].find_all("span")[1].text.split()[3])
+#     color = laptops[_].find_all("span")[1].text.split()[4]
+#     temp.append(diagonal)
+#     temp.append(name) 
+#     temp.append(color)
 
-with open('data.csv', 'w', newline='', encoding='utf-8') as f:
-    write = csv.writer(f, delimiter=';')
-   # write.writerow(fields)
-    write.writerows(temp)
+# with open('data.csv', 'w', newline='', encoding='utf-8') as f:
+#     write = csv.writer(f, delimiter=';')
+#    # write.writerow(fields)
+#     write.writerows(temp)
