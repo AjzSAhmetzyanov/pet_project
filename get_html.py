87bdbb2892daa607e5d6cdea01e4138b702244d7
Luigiket <luigiket@student.21-school.ru>
2023-06-22 11:43:40 +0300

    selenium

--- get_html.py
+++ get_html.py
@@ -1,128 +1,141 @@
-import aiohttp
-import aiofiles
-import asynch
-import asyncio
-import lxml
-from fake_useragent import UserAgent
-from bs4 import BeautifulSoup
-import requests
-import time
-from aiocsv import AsyncWriter
+# import aiohttp
+# import aiofiles
+# import asynch
+# import asyncio
+# import lxml
+# from fake_useragent import UserAgent
+# from bs4 import BeautifulSoup
+# import requests
+# import time
+# from aiocsv import AsyncWriter
 import csv
 
-ua = UserAgent()
+# ua = UserAgent()
 
-cookies = {
-    'lang': 'ru',
-    'city_path': 'moscow',
-    'phonesIdent': '4e957da8c854eddd1206b107327b2639a48bda9c0376c16fa93677d277038e54a%3A2%3A%7Bi%3A0%3Bs%3A11%3A%22phonesIdent%22%3Bi%3A1%3Bs%3A36%3A%22ebc14c93-0de7-4b96-81db-cca4277fefa1%22%3B%7D',
-    'cartUserCookieIdent_v3': '066ea3df7194c39af1c33108f73e99305445267de595e9d936bf1da6d0695758a%3A2%3A%7Bi%3A0%3Bs%3A22%3A%22cartUserCookieIdent_v3%22%3Bi%3A1%3Bs%3A36%3A%22e1838e05-bce1-3d5d-9639-1f76a4c394e5%22%3B%7D',
-    '_gcl_au': '1.1.1769402233.1687152784',
-    'rrpvid': '514393665157779',
-    'PHPSESSID': '77a3d6063def3e5ebd1ae05913401664',
-    'tmr_lvid': 'd31b8ac6291611090dbad5635c274b09',
-    'tmr_lvidTS': '1687152784780',
-    '_ym_uid': '1687152785781607891',
-    '_ym_d': '1687152785',
-    'rcuid': '648d7872ef0b6f85d3a3e9ef',
-    'current_path': '605bfdc517d7e9e23947448a9bf1ce16ac36b884434a3fdb10db053793c50392a%3A2%3A%7Bi%3A0%3Bs%3A12%3A%22current_path%22%3Bi%3A1%3Bs%3A115%3A%22%7B%22city%22%3A%2230b7c1f3-03fb-11dc-95ee-00151716f9f5%22%2C%22cityName%22%3A%22%5Cu041c%5Cu043e%5Cu0441%5Cu043a%5Cu0432%5Cu0430%22%2C%22method%22%3A%22manual%22%7D%22%3B%7D',
-    '_csrf': '2215566cad29db7d4b82817167355e5a41de8f04d7183110890bbb19874b7586a%3A2%3A%7Bi%3A0%3Bs%3A5%3A%22_csrf%22%3Bi%3A1%3Bs%3A32%3A%22EVOgtvpxzikgAfqy5rdvNj5er36VAV4v%22%3B%7D',
-    '_gid': 'GA1.2.1759671824.1687265202',
-    '_ym_isad': '2',
-    '_ym_visorc': 'b',
-    'qrator_jsr': '1687277798.116.6LhJ0P7tgdmyde1T-ic1kgpn909l96hptik8a6mg5eu1cgua2-00',
-    'qrator_jsid': '1687277798.116.6LhJ0P7tgdmyde1T-g9d7s2cr4ttv897d32fgrlsq3pmh8lrh',
-    'tmr_detect': '0%7C1687277860129',
-    '_gat_UA-8349380-2': '1',
-    '_gat': '1',
-    '_gat_%5Bobject%20Object%5D': '1',
-    'rr-testCookie': 'testvalue',
-    '_ga_FLS4JETDHW': 'GS1.1.1687271156.5.1.1687277922.55.0.0',
-    '_ga': 'GA1.1.487180927.1687152784',
-}
+# headers = {
+#     'Referer': 'https://www.dns-shop.ru/',
+#     'Upgrade-Insecure-Requests': '1',
+#     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
+#     'sec-ch-ua': '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
+#     'sec-ch-ua-mobile': '?0',
+#     'sec-ch-ua-platform': '"macOS"',
+# }
 
-headers = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'Accept-Language': 'en-US,en;q=0.9,ru;q=0.8',
-    'Connection': 'keep-alive',
-    # 'Cookie': 'lang=ru; city_path=moscow; phonesIdent=4e957da8c854eddd1206b107327b2639a48bda9c0376c16fa93677d277038e54a%3A2%3A%7Bi%3A0%3Bs%3A11%3A%22phonesIdent%22%3Bi%3A1%3Bs%3A36%3A%22ebc14c93-0de7-4b96-81db-cca4277fefa1%22%3B%7D; cartUserCookieIdent_v3=066ea3df7194c39af1c33108f73e99305445267de595e9d936bf1da6d0695758a%3A2%3A%7Bi%3A0%3Bs%3A22%3A%22cartUserCookieIdent_v3%22%3Bi%3A1%3Bs%3A36%3A%22e1838e05-bce1-3d5d-9639-1f76a4c394e5%22%3B%7D; _gcl_au=1.1.1769402233.1687152784; rrpvid=514393665157779; PHPSESSID=77a3d6063def3e5ebd1ae05913401664; tmr_lvid=d31b8ac6291611090dbad5635c274b09; tmr_lvidTS=1687152784780; _ym_uid=1687152785781607891; _ym_d=1687152785; rcuid=648d7872ef0b6f85d3a3e9ef; current_path=605bfdc517d7e9e23947448a9bf1ce16ac36b884434a3fdb10db053793c50392a%3A2%3A%7Bi%3A0%3Bs%3A12%3A%22current_path%22%3Bi%3A1%3Bs%3A115%3A%22%7B%22city%22%3A%2230b7c1f3-03fb-11dc-95ee-00151716f9f5%22%2C%22cityName%22%3A%22%5Cu041c%5Cu043e%5Cu0441%5Cu043a%5Cu0432%5Cu0430%22%2C%22method%22%3A%22manual%22%7D%22%3B%7D; _csrf=2215566cad29db7d4b82817167355e5a41de8f04d7183110890bbb19874b7586a%3A2%3A%7Bi%3A0%3Bs%3A5%3A%22_csrf%22%3Bi%3A1%3Bs%3A32%3A%22EVOgtvpxzikgAfqy5rdvNj5er36VAV4v%22%3B%7D; _gid=GA1.2.1759671824.1687265202; _ym_isad=2; _ym_visorc=b; qrator_jsr=1687277798.116.6LhJ0P7tgdmyde1T-ic1kgpn909l96hptik8a6mg5eu1cgua2-00; qrator_jsid=1687277798.116.6LhJ0P7tgdmyde1T-g9d7s2cr4ttv897d32fgrlsq3pmh8lrh; tmr_detect=0%7C1687277860129; _gat_UA-8349380-2=1; _gat=1; _gat_%5Bobject%20Object%5D=1; rr-testCookie=testvalue; _ga_FLS4JETDHW=GS1.1.1687271156.5.1.1687277922.55.0.0; _ga=GA1.1.487180927.1687152784',
-    'Referer': 'https://www.dns-shop.ru/catalog/eb53394d63f878ee/noutbuki-i-aksessuary/',
-    'Sec-Fetch-Dest': 'document',
-    'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-Site': 'same-origin',
-    'Sec-Fetch-User': '?1',
-    'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
-    'sec-ch-ua': '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"macOS"',
-}
+# temp = []
 
-temp = []
-
-async def get_page_data(session, page):
-    url = f"https://www.dns-shop.ru/catalog/17a892f816404e77/noutbuki/?p={page}"
-    async with session.get(url=url, headers=headers, cookies=cookies) as response:
-        response_text = await response.text()
-        soup = BeautifulSoup(response_text, 'lxml')
+# async def get_page_data(session, page):
+#     url = f"https://www.dns-shop.ru/catalog/17a892f816404e77/noutbuki/?p={page}"
+#     async with session.get(url=url, headers=headers) as response:
+#         response_text = await response.text()
+#         soup = BeautifulSoup(response_text, 'lxml')
         
-        laptops = soup.find_all("div", class_="catalog-product ui-button-widget")
-        for i in range(0, len(laptops)):
+#         laptops = soup.find_all("div", class_="catalog-product ui-button-widget")
+#         for i in range(0, len(laptops)):
             
-            try:
-                diagonal = laptops[i].find_all("span")[1].text.split()[0]
-                name = (laptops[i].find_all("span")[1].text.split()[2],laptops[i].find_all("span")[1].text.split()[3])
-                color = laptops[i].find_all("span")[1].text.split()[4]
-                matrix = (laptops[i].find_all("span")[1].text.split()[5],laptops[i].find_all("span")[1].text.split()[6])
+#             try:
+#                 diagonal = laptops[i].find_all("span")[1].text.split()[0]
+#                 name = (laptops[i].find_all("span")[1].text.split()[2],laptops[i].find_all("span")[1].text.split()[3])
+#                 color = laptops[i].find_all("span")[1].text.split()[4]
+#                 matrix = (laptops[i].find_all("span")[1].text.split()[5],laptops[i].find_all("span")[1].text.split()[6])
 
-                fields = ['diagonal', 'name', 'color', 'matrix', 'page']
-            except IndexError:
-                i += 1
+#                 fields = ['diagonal', 'name', 'color', 'matrix', 'page']
+#             except IndexError:
+#                 i += 1
                 
-                temp.append(
-                    [
-                    diagonal, name, color, matrix, page 
-                    ]
-                )
+#                 temp.append(
+#                     [
+#                     diagonal, name, color, matrix, page 
+#                     ]
+#                 )
 
-            with open('data.csv', 'w') as f:
-                # using csv.writer method from CSV package
-                write = csv.writer(f)
-                write.writerow(fields)
-                write.writerows(temp)
+#             with open('data.csv', 'w') as f:
+#                 # using csv.writer method from CSV package
+#                 write = csv.writer(f)
+#                 write.writerow(fields)
+#                 write.writerows(temp)
 
-            # async with aiofiles.open('data.csv', 'w') as f:
-            #     writer = AsyncWriter(f)
-            #     await writer.writerow(['diagonal', diagonal])
-            #     await writer.writerow(['name', name])
-            #     await writer.writerow(['color', color])
-            #     await writer.writerow(['matrix', matrix])
-            #     await writer.writerow(['page', page])
-
-
-
-async def gather_data():
-    start_time = time.time()
-    async with aiohttp.ClientSession() as session:
-        response = await session.get('https://www.dns-shop.ru/catalog/17a892f816404e77/noutbuki/', cookies=cookies, headers=headers)
-        soup = BeautifulSoup(await response.text(), 'lxml')
-        # pages_count = int(soup.find("div", class_="pagination-container").find_all("a")[-1].text)
-        tasks = []
-
-        for page in range(1, 3):
-
-                task = asyncio.create_task(get_page_data(session, page))
-                tasks.append(task)
-
-        await asyncio.gather(*tasks)
-        print(str(time.time() - start_time) + " : sec")
+#             # async with aiofiles.open('data.csv', 'w') as f:
+#             #     writer = AsyncWriter(f)
+#             #     await writer.writerow(['diagonal', diagonal])
+#             #     await writer.writerow(['name', name])
+#             #     await writer.writerow(['color', color])
+#             #     await writer.writerow(['matrix', matrix])
+#             #     await writer.writerow(['page', page])
 
 
-def main():
-    asyncio.run(gather_data())
-    print(temp)
+
+# async def gather_data():
+#     start_time = time.time()
+#     async with aiohttp.ClientSession() as session:
+#         response = await session.get('https://www.dns-shop.ru/catalog/17a892f816404e77/noutbuki/', headers=headers)
+#         soup = BeautifulSoup(await response.text(), 'lxml')
+#         # pages_count = int(soup.find("div", class_="pagination-container").find_all("a")[-1].text)
+#         tasks = []
+
+#         for page in range(1, 3):
+
+#                 task = asyncio.create_task(get_page_data(session, page))
+#                 tasks.append(task)
+
+#         await asyncio.gather(*tasks)
+#         print(str(time.time() - start_time) + " : sec")
 
 
-if __name__ == '__main__':
-    main()
+# def main():
+#     asyncio.run(gather_data())
+#     print(temp)
 
+
+# if __name__ == '__main__':
+#     main()
+
+# users_data = [
+#     ("user_name", "user_address"),
+#     ["user1", "address1"],
+#     ["user2", "address2"],
+#     ["user3", "address3"],
+# ]
+
+# with open("data.csv", "w") as file:
+#     writer = csv.writer(file)
+#     writer.writerows(
+#         users_data
+#     )
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium_stealth import stealth
+import time
+
+
+options = webdriver.ChromeOptions()
+options.add_argument("start-maximized")
+
+# options.add_argument("--headless")
+
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+driver = webdriver.Chrome(options=options)
+stealth(driver,
+        languages=["en-US", "en"],
+        vendor="Google Inc.",
+        platform="Win32",
+        webgl_vendor="Intel Inc.",
+        renderer="Intel Iris OpenGL Engine",
+        fix_hairline=True,
+        )
+
+for page in range(1,3):
+    domain = "https://www.russiadiscovery.ru/"
+    url = f"https://www.russiadiscovery.ru/tours/trekkingi/?page={page}"
+    driver.get(url)
+
+    blocks = driver.find_element(By.CLASS_NAME, "tourListUL")
+    posts = blocks.find_elements(By.TAG_NAME, "li")
+
+    for post in posts:
+        title = post.find_element(By.CLASS_NAME, "tourList__title").find_element(By.TAG_NAME, "a").get_attribute("href")
+        print(title)
+
+# time.sleep(5)
+# driver.quit()
